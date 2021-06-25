ddc1b0a4cfa2561c37cc41b38c543864c4b3493d
Dusan Bucan <dusanbzr@gmail.com>
2021-06-25 19:54:39 +0200

    updated stop-local-env.sh

--- stop-local-env.sh
+++ stop-local-env.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 docker rm -f $(docker ps -a -q)
+
+rm -rf ./Nistagram-Api-Gateway
+rm -rf ./nistagram-front
+rm -rf ./Nistagram-Auth
+rm -rf ./Nistagram-Campaign
+rm -rf ./Nistagram-Media
+rm -rf ./Nistagram-Search
+rm -rf ./Nistagram-Post
+rm -rf ./Nistagram-User
