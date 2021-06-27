7ecd470393b18bd36174f3affb3166c640141d14
Dusan Bucan <dusanbzr@gmail.com>
2021-06-27 02:06:42 +0200

    kafka added

--- start-local-env.sh
+++ start-local-env.sh
@@ -12,16 +12,16 @@ cp -r ../Nistagram-User ./Nistagram-User
 
 
 # ## mora ovako zbog [output clipped, log limit 1MiB reached]
-# docker buildx create --use --name larger_log --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=50000000
+docker buildx create --use --name larger_log --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=50000000
 
 # ## Build Nistagram-Api-Gateway microsrevice
-# docker buildx build --load --progress plain ./Nistagram-Api-Gateway --target gatewayRuntimeDev --tag gateway-nistagram
+docker buildx build --load --progress plain ./Nistagram-Api-Gateway --target gatewayRuntimeDev --tag gateway-nistagram
 
 # ## Build Auth microservice
-# docker buildx build --load --progress plain ./Nistagram-Auth --target nistagramAuthMicroserviceRuntimeDev --tag auth-service-nistagram
+docker buildx build --load --progress plain ./Nistagram-Auth --target nistagramAuthMicroserviceRuntimeDev --tag auth-service-nistagram
 
 # ## Build Campaign microsrevice
-# docker buildx build --load --progress plain ./Nistagram-Campaign --target nistagramCampaignMicroserviceRuntimeDev --tag campaign-service-nistagram
+docker buildx build --load --progress plain ./Nistagram-Campaign --target nistagramCampaignMicroserviceRuntimeDev --tag campaign-service-nistagram
 
 # ## Build Nistagram-Media microsrevice
 docker buildx build --load --progress plain ./Nistagram-Media --target nistagramMediaMicroserviceRuntimeDev --tag media-service-nistagram
@@ -35,8 +35,7 @@ docker buildx build --load --progress plain ./Nistagram-Search --target nistagra
 # ## Build Nistagram-User microsrevice
 docker buildx build --load --progress plain ./Nistagram-User --target nistagramUserMicroserviceRuntimeDev --tag user-service-nistagram
 
-
-echo "Finish builds"
+echo "Finished builds"
 
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f ./docker-compose.dev.yml --env-file ./config/.env.dev up -d
 
